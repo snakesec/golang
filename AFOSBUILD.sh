59e2ee64ba81e73574cc81bd5fea26c18ca6afa8
Weidsom Nascimento <source@snakesecurity.org>
2025-07-04 15:20:14 -0700

    Update golang version

--- AFOSBUILD.sh
+++ AFOSBUILD.sh
@@ -1,11 +1,11 @@
 rm -rf /usr/lib/go/
 
 if [ $(uname -m | grep 'x86_64') ]; then
-  wget https://go.dev/dl/go1.24.2.linux-amd64.tar.gz
-  tar -xvzf go1.24.2.linux-amd64.tar.gz
+  wget https://go.dev/dl/go1.24.4.linux-amd64.tar.gz
+  tar -xvzf go1.24.4.linux-amd64.tar.gz
   cp -Rf go/ /usr/lib/go
 else
-  wget https://go.dev/dl/go1.24.2.linux-arm64.tar.gz
-  tar -xvzf go1.24.2.linux-arm64.tar.gz
+  wget https://go.dev/dl/go1.24.4.linux-arm64.tar.gz
+  tar -xvzf go1.24.4.linux-arm64.tar.gz
   cp -Rf go/ /usr/lib/go
 fi
